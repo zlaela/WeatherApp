0a147c14ca1f72981eaca7a41254bd46549b6985
zlaela <zlaela@users.noreply.github.com>
2023-04-06 13:42:38 -0500

    add City response data classes

---
+++ data/src/main/java/com/example/data/api/response/city/City.kt
@@ -0,0 +1,20 @@
+package com.example.data.api.response.city
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class City(
+    @Json(name = "name")
+    val name: String?,
+    @Json(name = "lat")
+    val lat: Double?,
+    @Json(name = "lon")
+    val lon: Double?,
+    @Json(name = "country")
+    val country: String?,
+    @Json(name = "state")
+    val state: String?,
+    @Json(name = "local_names")
+    val localNames: LocalNames? = null
+)
