c0b6f6d8968b8f26473ba2b61025f889dc253d5b
zlaela <zlaela@users.noreply.github.com>
2023-04-06 15:42:23 -0500

    Update GeoApi.kt to return Cities when more than 1 City can be expected

--- data/src/main/java/com/example/data/api/GeoApi.kt
+++ data/src/main/java/com/example/data/api/GeoApi.kt
@@ -1,9 +1,10 @@
 package com.example.data.api
 
+import com.example.data.api.response.city.Cities
 import com.example.data.api.response.city.City
 import kotlinx.coroutines.Deferred
 
 interface GeoApi {
-    suspend fun searchZipAsync(zip: String): Deferred<List<City>>
-    suspend fun searchCityAsync(city: String): Deferred<List<City>>
+    suspend fun searchZipAsync(zip: String): Deferred<City>
+    suspend fun searchCityAsync(city: String): Deferred<Cities>
 }
