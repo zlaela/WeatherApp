7924aad52f05cf53374e29b25d4de4c16d2b3af8
zlaela <zlaela@users.noreply.github.com>
2023-04-06 21:37:01 -0500

    Updated mappers to map response to forecast object

--- data/src/main/java/com/example/data/domain/Mappers.kt
+++ data/src/main/java/com/example/data/domain/Mappers.kt
@@ -1,6 +1,7 @@
 package com.example.data.domain
 
 import com.example.data.api.response.weather.CurrentWeatherResponse
+import com.example.data.api.response.weather.FiveDayForecastResponse
 
 fun CurrentWeatherResponse.mapToCurrentWeather(): CurrentWeather =
     CurrentWeather(
@@ -14,3 +15,20 @@ fun CurrentWeatherResponse.mapToCurrentWeather(): CurrentWeather =
         this.weather.first().icon,
         this.weather.first().description
     )
+
+fun FiveDayForecastResponse.mapToForecast(): List<Forecast> =
+    with(mutableListOf<Forecast>()) {
+        this@mapToForecast.forecasts.forEach {
+            add(Forecast(
+                it.dt,
+                it.dtTxt,
+                it.main.tempMin,
+                it.main.tempMax,
+                it.partOfDay.partOfDay,
+                it.weather.first().icon,
+                it.weather.first().main,
+                it.weather.first().description
+            ))
+        }
+        this
+    }.toList()
