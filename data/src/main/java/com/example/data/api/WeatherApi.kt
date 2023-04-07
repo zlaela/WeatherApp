9e52361c14d216ce9a9a6ea489734305ffd60904
zlaela <zlaela@users.noreply.github.com>
2023-04-06 21:34:12 -0500

    added WeatherApi.kt getForecast()

--- data/src/main/java/com/example/data/api/WeatherApi.kt
+++ data/src/main/java/com/example/data/api/WeatherApi.kt
@@ -1,9 +1,11 @@
 package com.example.data.api
 
 import com.example.data.api.response.weather.CurrentWeatherResponse
+import com.example.data.api.response.weather.FiveDayForecastResponse
 import com.example.data.domain.City
 import kotlinx.coroutines.Deferred
 
 interface WeatherApi {
     suspend fun getCurrentWeather(city: City): Deferred<CurrentWeatherResponse>
+    suspend fun getForecast(city: City): Deferred<FiveDayForecastResponse>
 }
