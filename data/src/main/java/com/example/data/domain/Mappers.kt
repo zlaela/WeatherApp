7c81ab6d4703aa149bd6698286d4e83b5c8c44aa
zlaela <zlaela@users.noreply.github.com>
2023-04-08 18:56:42 -0500

    Handle nullables, add default values in Mappers.kt

--- data/src/main/java/com/example/data/domain/Mappers.kt
+++ data/src/main/java/com/example/data/domain/Mappers.kt
@@ -5,29 +5,29 @@ import com.example.data.api.response.weather.FiveDayForecastResponse
 
 fun CurrentWeatherResponse.mapToCurrentWeather(): CurrentWeather =
     CurrentWeather(
-        this.name,
-        this.coord.lat,
-        this.coord.lon,
-        this.main.tempMin,
-        this.main.tempMax,
-        this.main.temp,
-        this.main.feelsLike,
-        this.weather.first().icon,
-        this.weather.first().description
+        this.name ?: "",
+        this.coord?.lat ?: 0.0,
+        this.coord?.lon ?: 0.0,
+        this.main?.tempMin ?: 0.0,
+        this.main?.tempMax ?: 0.0,
+        this.main?.temp ?: 0.0,
+        this.main?.feelsLike ?: 0.0,
+        this.weather?.first()?.icon ?: "",
+        this.weather?.first()?.description ?: ""
     )
 
 fun FiveDayForecastResponse.mapToForecast(): List<Forecast> =
     with(mutableListOf<Forecast>()) {
-        this@mapToForecast.forecasts.forEach {
+        this@mapToForecast.forecasts?.forEach {
             add(Forecast(
-                it.dt,
-                it.dtTxt,
-                it.main.tempMin,
-                it.main.tempMax,
-                it.partOfDay.partOfDay,
-                it.weather.first().icon,
-                it.weather.first().main,
-                it.weather.first().description
+                it.dt ?: 0,
+                it.dtTxt ?: "",
+                it.main?.tempMin ?: 0.0,
+                it.main?.tempMax ?: 0.0,
+                it.partOfDay?.partOfDay ?: "",
+                it.weather?.first()?.icon ?: "",
+                it.weather?.first()?.main ?: "",
+                it.weather?.first()?.description ?: ""
             ))
         }
         this
