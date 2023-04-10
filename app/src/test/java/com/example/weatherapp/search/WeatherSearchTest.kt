36a712a70923b4857789fe7ed609edb804ad45b1
zlaela <zlaela@users.noreply.github.com>
2023-04-10 13:56:01 -0500

    Weather search feature updates the UI with the weather for a saved city, if it exists

--- app/src/test/java/com/example/weatherapp/search/WeatherSearchTest.kt
+++ app/src/test/java/com/example/weatherapp/search/WeatherSearchTest.kt
@@ -4,22 +4,28 @@ import androidx.lifecycle.Observer
 import com.example.data.api.WeatherApi
 import com.example.data.api.response.weather.CurrentWeatherResponse
 import com.example.data.api.response.weather.FiveDayForecastResponse
-import com.example.data.domain.City
-import com.example.data.domain.mapToCurrentWeather
-import com.example.data.domain.mapToForecast
+import com.example.data.domain.*
+import com.example.data.repository.DataStoreRepository
 import com.example.data.repository.WeatherSearchRepository
 import com.example.data.search.WeatherResult
+import com.example.data.store.DataStoreState
+import com.example.data.store.PreferencesDataSource
+import com.example.data.store.UserPreferences
 import com.example.weatherapp.ExecutionExtension
+import com.example.weatherapp.SpyUiController
 import com.example.weatherapp.TestCoroutineDispatchers
 import com.example.weatherapp.TestHelpers
 import com.example.weatherapp.viewmodel.WeatherSearchViewModel
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verifySequence
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -27,17 +33,21 @@ import org.junit.jupiter.api.extension.ExtendWith
 /** Weather search feature will
  * Handle successful weather search result
  * Handle unsuccessful weather search result
+ * Look for a saved location in datastore and update weather if it exists
  */
 @ExtendWith(ExecutionExtension::class, MockKExtension::class)
 class WeatherSearchTest {
     @MockK
+    private lateinit var dataSource: PreferencesDataSource
+
+    @MockK
     private lateinit var weatherApi: WeatherApi
 
     @MockK
-    private lateinit var weatherDeferred: Deferred<CurrentWeatherResponse>
+    private lateinit var forecastDeferred: Deferred<FiveDayForecastResponse>
 
     @MockK
-    private lateinit var forecastDeferred: Deferred<FiveDayForecastResponse>
+    private lateinit var weatherDeferred: Deferred<CurrentWeatherResponse>
 
     @MockK
     private lateinit var weatherLiveDataObserver: Observer<WeatherResult>
@@ -47,26 +57,82 @@ class WeatherSearchTest {
     private val hideLoading: WeatherResult = WeatherResult.HideLoading
     private lateinit var weatherSearchViewModel: WeatherSearchViewModel
 
+    private lateinit var uiController: WeatherSearchUiController
+
     @BeforeEach
     fun setUp() {
         val dispatchers = TestCoroutineDispatchers()
         val weatherRepository = WeatherSearchRepository(weatherApi)
-        weatherSearchViewModel = WeatherSearchViewModel(dispatchers, weatherRepository)
+        val dataStoreRepository = DataStoreRepository(dataSource)
+
+        weatherSearchViewModel =
+            WeatherSearchViewModel(dispatchers, weatherRepository, dataStoreRepository)
+        uiController = WeatherSearchUiController().also {
+            it.viewModel = weatherSearchViewModel
+        }
     }
 
     @Test
-    fun `performs weather lookup for given city`() = runBlocking {
-        val expectedResponse = getWeatherResponse()
-        val expectedWeather = expectedResponse.mapToCurrentWeather()
+    fun `looks for saved city in prefs on startup`() = runBlocking {
+        val emptyPrefs = UserPreferences()
+        coEvery { dataSource.getPreferences() }.answers { emptyPrefs }
 
-        // Ensure every change is emitted
-        every { weatherLiveDataObserver.onChanged(any()) }.answers { }
-        coEvery { weatherApi.getCurrentWeather(someCity.name) }.returns(weatherDeferred)
-        coEvery { weatherDeferred.await() }.coAnswers { expectedResponse }
+        // When the view is created
+        uiController.onCreate()
 
-        weatherSearchViewModel.weatherLiveData.observeForever(weatherLiveDataObserver)
+        // The controller subscribes to prefs updates
+        Assertions.assertTrue(weatherSearchViewModel.getPrefsOnStart.hasActiveObservers())
+        coVerify(exactly = 1) { dataSource.getPreferences() }
 
-        // When the viewModel searches for a city
+        // The response state is as expected for empty prefs
+        val successState = DataStoreState.GetPreferencesSuccess(null, null)
+        assertEquals(successState, uiController.dataStoreStates.first())
+    }
+
+    @Test
+    fun `updates weather for last location if found on startup`() = runBlocking {
+        val cityPrefs = UserPreferences(null, someCity.name, someCity.lat, someCity.lon)
+        val expectedWeather = setUpWeatherResults()
+
+        // Mimic datastore returns prefs
+        coEvery { dataSource.getPreferences() }.coAnswers { cityPrefs }
+
+        // When the view is created
+        uiController.onCreate()
+
+        // The datastore returns the expected preferences
+        coVerify(exactly = 1) { dataSource.getPreferences() }
+        val successState = DataStoreState.GetPreferencesSuccess(someCity, cityPrefs.country)
+        assertEquals(successState, uiController.dataStoreStates.first())
+
+        // And the viewmodel updates the weather for the city
+        verifySequence {
+            weatherLiveDataObserver.onChanged(showLoading)
+            weatherLiveDataObserver.onChanged(WeatherResult.WeatherSuccess(expectedWeather))
+            weatherLiveDataObserver.onChanged(hideLoading)
+        }
+    }
+
+    @Test
+    fun `performs forecast lookup for a given city`() = runBlocking {
+        val expectedForecast = setUpForecastResults()
+
+        // When the viewModel searches for the forecast
+        weatherSearchViewModel.getForecast(someCity)
+
+        // Verify that livedata emits loading state, weather result, and hide loading states in order
+        verifySequence {
+            weatherLiveDataObserver.onChanged(showLoading)
+            weatherLiveDataObserver.onChanged(WeatherResult.ForecastSuccess(expectedForecast))
+            weatherLiveDataObserver.onChanged(hideLoading)
+        }
+    }
+
+    @Test
+    fun `performs weather lookup for a given city`() = runBlocking {
+        val expectedWeather = setUpWeatherResults()
+
+        // When the viewModel searches for the weather
         weatherSearchViewModel.getWeatherFor(someCity)
 
         // Verify that livedata emits loading state, weather result, and hide loading states in order
@@ -77,28 +143,32 @@ class WeatherSearchTest {
         }
     }
 
-    @Test
-    fun `performs forecast lookup for a given city`() = runBlocking {
-        val expectedResponse = getForecastResponse()
-        val expectedForecast = expectedResponse.mapToForecast()
+    private fun setUpForecastResults(): List<Forecast> {
+        val expectedForecastResponse = getForecastResponse()
 
         // Ensure every change is emitted
         every { weatherLiveDataObserver.onChanged(any()) }.answers { }
-        coEvery { weatherApi.getForecast(someCity.lat, someCity.lon) }.coAnswers { forecastDeferred }
-        coEvery { forecastDeferred.await() }.coAnswers { expectedResponse }
+        coEvery {
+            weatherApi.getForecast(someCity.lat,
+                someCity.lon)
+        }.coAnswers { forecastDeferred }
+        coEvery { forecastDeferred.await() }.coAnswers { expectedForecastResponse }
 
         weatherSearchViewModel.weatherLiveData.observeForever(weatherLiveDataObserver)
 
-        // When the viewModel searches for a city
-        weatherSearchViewModel.getForecast(someCity)
+        return expectedForecastResponse.mapToForecast()
+    }
+    private fun setUpWeatherResults(): CurrentWeather {
+        val expectedResponse = getWeatherResponse()
 
-        // Verify that livedata emits loading state, weather result, and hide loading states in order
-        verifySequence {
-            weatherLiveDataObserver.onChanged(showLoading)
-            weatherLiveDataObserver.onChanged(WeatherResult.ForecastSuccess(expectedForecast))
-            weatherLiveDataObserver.onChanged(hideLoading)
-        }
+        // Ensure every change is emitted
+        every { weatherLiveDataObserver.onChanged(any()) }.answers { }
+        coEvery { weatherApi.getCurrentWeather(someCity.name) }.coAnswers { weatherDeferred }
+        coEvery { weatherDeferred.await() }.coAnswers { expectedResponse }
+        // Viewmodel updates subscribed
+        weatherSearchViewModel.weatherLiveData.observeForever(weatherLiveDataObserver)
 
+        return expectedResponse.mapToCurrentWeather()
     }
 
     // TODO: This is duplicated and should  be moved to a common testing package
@@ -108,4 +178,27 @@ class WeatherSearchTest {
     // TODO: This is duplicated and should  be moved to a common testing package
     private fun getForecastResponse(): FiveDayForecastResponse =
         TestHelpers.getWeatherResponse(FiveDayForecastResponse::class.java, "forecast.json")
+
+    class WeatherSearchUiController : SpyUiController() {
+        lateinit var viewModel: WeatherSearchViewModel
+        val dataStoreStates = mutableListOf<DataStoreState>()
+
+        override fun onCreate() {
+            super.onCreate()
+            viewModel.getPrefsOnStart.observe(this) { dataStoreState ->
+                dataStoreStates.add(dataStoreState)
+                when (dataStoreState) {
+                    is DataStoreState.GetPreferencesSuccess -> {
+                        dataStoreState.city
+                            ?.let { nnCity ->
+                                viewModel.getWeatherFor(nnCity)
+                            }
+                    }
+                    else -> {
+                        /** TODO: Nothing for now. Maybe device location search? */
+                    }
+                }
+            }
+        }
+    }
 }
