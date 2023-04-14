b7bf74a56d8b1a2e652f15495ebe5a653c3fb0cb
zlaela <zlaela@users.noreply.github.com>
2023-04-14 12:29:29 -0500

    GeoApi.kt returns list of CityItem instead of Cities.

--- data/src/main/java/com/example/data/api/GeoApi.kt
+++ data/src/main/java/com/example/data/api/GeoApi.kt
@@ -1,6 +1,5 @@
 package com.example.data.api
 
-import com.example.data.api.response.city.Cities
 import com.example.data.api.response.city.CityItem
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
@@ -9,11 +8,12 @@ import retrofit2.http.Query
 interface GeoApi {
     @GET("geo/1.0/zip")
     fun searchZipAsync(
-        @Query("zip") zip: String
+        @Query("zip") zip: String,
     ): Deferred<CityItem>
 
     @GET("geo/1.0/direct")
     fun searchCityAsync(
-        @Query("q") city: String
-    ): Deferred<Cities>
+        @Query("q") city: String,
+        @Query("limit") limit: Int = 5,
+    ): Deferred<List<CityItem>>
 }
