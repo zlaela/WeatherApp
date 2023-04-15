5f922bc0e91adf3f2ce28cb7d5c825ad1ba0a8b9
zlaela <zlaela@users.noreply.github.com>
2023-04-15 17:56:37 -0500

    Search bar with leading and trailing icons, callbacks, dismisses keyboard on enter/search, test tag

--- app/src/main/java/com/example/weatherapp/ui/TestTags.kt
+++ app/src/main/java/com/example/weatherapp/ui/TestTags.kt
@@ -4,4 +4,5 @@ object TestTags {
     const val SPLASH = "SPLASH"
     const val MAIN = "MAIN"
     const val SPINNER = "SPINNER"
+    const val SEARCH_FIELD = "SEARCH_FIELD"
 }
---
+++ app/src/main/java/com/example/weatherapp/ui/composable/SearchBar.kt
@@ -0,0 +1,78 @@
+package com.example.weatherapp.ui.composable
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.unit.dp
+import com.example.weatherapp.ui.TestTags
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SearchBar(
+    enabled: Boolean,
+    isError: Boolean,
+    searchHint: String,
+    leadingIcon: @Composable() (() -> Unit)? = null,
+    trailingIcon: @Composable() (() -> Unit)? = null,
+    onLeadingIconClick: () -> Unit = {},
+    onTrailingIconClick: () -> Unit = {},
+    onSearchFieldBlank: () -> Unit = {},
+    onKeyboardDone: (String) -> Unit = {},
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var textFieldText by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .padding(start = 4.dp, end = 4.dp)
+            .fillMaxWidth()
+            .testTag(TestTags.SEARCH_FIELD),
+        value = textFieldText,
+        onValueChange = { searchTerm ->
+            if (searchTerm.isBlank() || searchTerm.isEmpty()) {
+                onSearchFieldBlank()
+            }
+            textFieldText = searchTerm
+        },
+        label = { Text(text = searchHint) },
+        enabled = enabled,
+        leadingIcon = {
+            leadingIcon?.let { icon ->
+                IconButton(
+                    onClick = { onLeadingIconClick() },
+                    content = { icon() }
+                )
+            }
+        },
+        isError = isError,
+        trailingIcon = {
+            trailingIcon?.let { icon ->
+                IconButton(
+                    onClick = { onTrailingIconClick() },
+                    content = { icon() }
+                )
+            }
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                // Send the text field content to be processed
+                onKeyboardDone(textFieldText)
+                // Hide keyboard
+                keyboardController?.hide()
+            }
+        )
+    )
+}
