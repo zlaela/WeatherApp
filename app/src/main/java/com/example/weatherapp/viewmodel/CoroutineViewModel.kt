168e0d2a1ca896926aeaaebc1b1abc21f9bebc64
zlaela <zlaela@users.noreply.github.com>
2023-04-27 10:55:27 -0500

    Added exception handler for now

--- app/src/main/java/com/example/weatherapp/viewmodel/CoroutineViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/CoroutineViewModel.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
@@ -17,11 +18,20 @@ interface CoroutineDispatchers {
 abstract class CoroutineViewModel(
     private val dispatchers: CoroutineDispatchers
 ) : ViewModel(), CoroutineScope {
+
+    //TODO: remove or fix
+    private val coroutineExceptionHandler = CoroutineExceptionHandler{ _, throwable ->
+        throwable.printStackTrace()
+    }
+
     // Parent of all child coroutines originating from viewmodels that extend this
     private val job = Job()
 
     override val coroutineContext: CoroutineContext
         get() = dispatchers.ui + job
+
+    val exceptionContext: CoroutineContext
+            get() = coroutineContext + coroutineExceptionHandler
 
     override fun onCleared() {
         job.cancel()
