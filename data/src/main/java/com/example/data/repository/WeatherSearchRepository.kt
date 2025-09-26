93434811a00ffbc4b915b3073c1ef217cf19f96d
zlaela <zlaela@users.noreply.github.com>
2025-09-26 11:52:53 -0500

    Catch retrofit httpException, add more detailed failure reason

--- data/src/main/java/com/example/data/repository/WeatherSearchRepository.kt
+++ data/src/main/java/com/example/data/repository/WeatherSearchRepository.kt
@@ -4,29 +4,35 @@ import com.example.data.api.WeatherApi
 import com.example.data.domain.City
 import com.example.data.domain.mapToCurrentWeather
 import com.example.data.domain.mapToForecast
-import com.example.data.exception.HttpException
 import com.example.data.search.WeatherResult
+import retrofit2.HttpException
+import java.net.UnknownHostException
 
 class WeatherSearchRepository(
     private val weatherApi: WeatherApi
 ) : WeatherRepository {
 
     override suspend fun getCurrentWeather(someCity: City): WeatherResult {
-        return try {
-            val resultAsync = weatherApi.getCurrentWeather(someCity.name).await()
-            WeatherResult.WeatherSuccess(resultAsync.mapToCurrentWeather())
-        } catch (ex: HttpException) {
-            WeatherResult.Failure("Bad")
+        return runCatching {
+            val result = weatherApi.getCurrentWeather(someCity.name).await()
+            return WeatherResult.WeatherSuccess(result.mapToCurrentWeather())
+        }.getOrElse { throwable ->
+            WeatherResult.Failure(reasonFor(throwable))
         }
     }
 
     override suspend fun getForecast(someCity: City): WeatherResult {
-        return try {
-            val resultAsync = weatherApi.getForecast(someCity.lat, someCity.lon).await()
-            val forecasts = resultAsync.mapToForecast()
-            WeatherResult.ForecastSuccess(forecasts)
-        } catch (ex: HttpException) {
-            WeatherResult.Failure("Bad")
+        return runCatching {
+            val result = weatherApi.getForecast(someCity.lat, someCity.lon).await()
+            WeatherResult.ForecastSuccess(result.mapToForecast())
+        }.getOrElse { throwable ->
+            WeatherResult.Failure(reasonFor(throwable))
         }
+    }
+
+    fun reasonFor(throwable: Throwable) = when (throwable) {
+        is UnknownHostException ->  "Network error"
+        is HttpException -> "HTTP error ${throwable.code()}"
+        else -> "Network error"
     }
 }
