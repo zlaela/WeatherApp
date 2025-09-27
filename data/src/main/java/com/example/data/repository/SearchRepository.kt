8dc5fa14a0d4173e88e33582f5c971fec673f94c
zlaela <zlaela@users.noreply.github.com>
2025-09-27 15:08:16 -0500

    Fix crash when searching and network is unavailable, update test

--- data/src/main/java/com/example/data/repository/SearchRepository.kt
+++ data/src/main/java/com/example/data/repository/SearchRepository.kt
@@ -8,21 +8,21 @@ import com.example.data.search.SearchState
 class SearchRepository(private val geoApi: GeoApi) {
     // City search can return 0 to 5 cities
     suspend fun searchCity(someCity: String): SearchState {
-        return try {
+        return runCatching {
             val result = geoApi.searchCityAsync(someCity).await().mapToCitiesList()
             SearchState.CitiesResult(result)
-        } catch (ex: retrofit2.HttpException) {
-            SearchState.Failure("Failed")
+        }.getOrElse { throwable ->
+            SearchState.Failure(reasonFor(throwable))
         }
     }
 
     // Zip search returns 0 or 1 cities
     suspend fun searchZip(someZip: String): SearchState {
-        return try {
+        return runCatching {
             val result = geoApi.searchZipAsync(someZip).await().mapToCity()
             SearchState.ZipResult(result)
-        } catch (ex: retrofit2.HttpException) {
-            SearchState.Failure("Failed")
+        }.getOrElse { throwable ->
+            SearchState.Failure(reasonFor(throwable))
         }
     }
 }
