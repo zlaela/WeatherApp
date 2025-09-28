a1d97e03941c7de33407954ea5ec34989ac23ffa
zlaela <zlaela@users.noreply.github.com>
2025-09-28 15:19:09 -0500

    UI cards, string resources

--- app/src/main/java/com/example/weatherapp/ui/composable/ForecastCard.kt
+++ app/src/main/java/com/example/weatherapp/ui/composable/ForecastCard.kt
@@ -1,10 +1,13 @@
 package com.example.weatherapp.ui.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,23 +16,32 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.data.domain.DayNightForecast
+import com.example.data.domain.Forecast
 import com.example.data.search.ForecastResult
 import com.example.weatherapp.R
 import com.example.weatherapp.ui.TestTags
+import com.example.weatherapp.ui.utils.getWeatherIconResource
 
 @Composable
 fun ForecastCard(
@@ -38,6 +50,7 @@ fun ForecastCard(
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(top = 8.dp)
             .testTag(TestTags.CITY_FORECAST)
     ) {
         when (val state = forecastStates.value) {
@@ -126,7 +139,9 @@ private fun ErrorForecastState(reason: String) {
 @Composable
 private fun ForecastState(locationForecasts: List<DayNightForecast>) {
     LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(locationForecasts.size) { idx ->
@@ -144,23 +159,143 @@ private fun ForecastState(locationForecasts: List<DayNightForecast>) {
 }
 
 @Composable
-fun DayNightForecastCard(dayNightForecast: DayNightForecast) {
+private fun DayNightForecastCard(dayNightForecast: DayNightForecast) {
     Row(
         modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         // Day forecast
         dayNightForecast.dayForecast?.let { dayForecast ->
-            Text("$dayForecast", modifier = Modifier.width(50.dp))
+            DayNightItem(
+                forecast = dayForecast,
+                modifier = Modifier.weight(1f)
+            )
         }
 
         // Divider
         if (dayNightForecast.dayForecast != null && dayNightForecast.nightForecast != null) {
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.width(16.dp))
         }
 
         // Night forecast
         dayNightForecast.nightForecast?.let { nightForecast ->
-            Text("$nightForecast", modifier = Modifier.width(50.dp))
+            DayNightItem(
+                forecast = nightForecast,
+                modifier = Modifier.weight(1f),
+                backgroundColor = MaterialTheme.colorScheme.primary
+            )
         }
+    }
+}
+
+@Composable
+private fun DayNightItem(
+    forecast: Forecast,
+    modifier: Modifier,
+    backgroundColor: Color = MaterialTheme.colorScheme.inversePrimary
+) {
+    val iconResource = remember { getWeatherIconResource(forecast.icon) }
+    
+    // Use appropriate text colors based on background
+    val textColor = if (backgroundColor == MaterialTheme.colorScheme.primary) {
+        MaterialTheme.colorScheme.onPrimary
+    } else {
+        MaterialTheme.colorScheme.onBackground
+    }
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = backgroundColor,
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp
+                ),
+
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+            Text(
+                text = forecast.description.capitalize(Locale.current),
+                style = MaterialTheme.typography.labelLarge,
+                fontWeight = FontWeight.SemiBold,
+                color = textColor,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Temperature range
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TempRange( R.string.high, forecast.tempMax.toInt(), textColor)
+
+                Image(
+                    painter = painterResource(id = iconResource),
+                    contentDescription = stringResource(R.string.weather_icon),
+                    modifier = Modifier.weight(1f).fillMaxHeight().aspectRatio(1f)
+                )
+                TempRange(R.string.low, forecast.tempMin.toInt(), textColor)
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Humidity and chance of rain
+            HumidityRain(forecast.humidity, forecast.chanceOfRain.toInt(), textColor)
+        }
+    }
+}
+
+@Composable
+private fun TempRange(
+    tempRes: Int,
+    temp: Int,
+    textColor: Color = MaterialTheme.colorScheme.onBackground
+) {
+    Column(modifier = Modifier.padding(4.dp), verticalArrangement = Arrangement.SpaceAround) {
+        Text(
+            text = stringResource(tempRes),
+            style = MaterialTheme.typography.labelMedium,
+            color = textColor
+        )
+
+        Text(
+            text = stringResource(R.string.temp_f, temp),
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Bold,
+            color =textColor
+        )
+    }
+}
+
+@Composable
+private fun HumidityRain(
+    humidity: Int,
+    rain: Int,
+    textColor: Color = MaterialTheme.colorScheme.onBackground
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 4.dp, end = 4.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.humidity_pct, humidity),
+            style = MaterialTheme.typography.bodySmall,
+            color = textColor
+        )
+        Text(
+            text = stringResource(R.string.chance_rain_pct, rain),
+            style = MaterialTheme.typography.bodySmall,
+            color = textColor
+        )
     }
 }
